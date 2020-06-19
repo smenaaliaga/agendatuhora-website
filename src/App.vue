3a834f768da300abb0956a1534ffb40d2bcfe3d6
smenaaliaga <smenaaliaga@gmail.com>
2020-06-19 01:05:50 -0400

    Se añade Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app>
       <Toolbar />
-      <Home />
+      <router-view></router-view>
       <Footer />
   </v-app>
 </template>
 
 <script>
   import Toolbar from './components/Toolbar';
-  import Home from './components/Home';
   import Footer from './components/Footer';
 
   export default {
     name: 'App',
     components: {
       Toolbar,
-      Home,
       Footer
     }
   };
--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import { store } from './store/store';
+import VueRouter from 'vue-router';
+import {routes} from './routes';
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes
+});
 
 new Vue({
   vuetify,
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
